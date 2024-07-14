a7b30a5c6269910579776e78696cb7837d028455
Bhaskara G Badiger <57971971+BhaskaraGB@users.noreply.github.com>
2024-07-14 14:16:38 -0500

    Update Volcano_ATAC_Peaks.R

--- Volcano_ATAC_Peaks.R
+++ Volcano_ATAC_Peaks.R
@@ -26,40 +26,12 @@ processed_data <- atac_data %>%
                           ifelse(log2ratio >= 0, "Increased", "Decreased"),
                           "Non-significant"))
 
-# # Label genes based on differential expression
-# processed_data$delabel <- NA
-# processed_data$delabel[processed_data$DiffExp != "Non-significant"] <- processed_data$Gene[processed_data$DiffExp != "Non-significant"]
-# 
-# # Remove rows with NA in delabel
-# labeled_data <- processed_data %>% na.omit(delabel)
-# 
-# # Sort data by log2ratio for up and down-regulated genes
-# df_sorted_log2ratio_Down <- arrange(labeled_data, log2ratio)
-# df_sorted_log2ratio_Up <- arrange(labeled_data, desc(log2ratio))
-# 
-# # Select top 30 down-regulated and top 50 up-regulated genes by log2ratio
-# df_top30_log2ratio_Down <- slice(df_sorted_log2ratio_Down, 1:30)
-# df_top50_log2ratio_Up <- slice(df_sorted_log2ratio_Up, 1:50)
-# 
-# # Sort data by FDR for up and down-regulated genes
-# df_sorted_FDR_Down <- arrange(labeled_data, FDR)
-# df_sorted_FDR_Up <- arrange(labeled_data, desc(FDR))
-# 
-# # Select top 50 genes by FDR for up and down-regulated genes
-# df_top50_FDR_Down <- slice(df_sorted_FDR_Down, 1:50)
-# df_top50_FDR_Up <- slice(df_sorted_FDR_Up, 1:50)
-
 # Plot data with ggplot2
 volcano_plot <- ggplot(processed_data, aes(log2ratio, -log10(FDR), color = DiffExp)) +
   geom_point() +
   scale_color_manual(values = c("blue", "red", "darkgrey"), name = NULL) +
   geom_vline(xintercept = c(-0.58, 0.58), col = "black", lwd = 0.5, lty = 2) +
   geom_hline(yintercept = -log10(0.05), col = "black", lwd = 0.5, lty = 2) +             
-  # Add gene labels (uncomment the following lines if needed)
-  # geom_text_repel(data = df_top30_log2ratio_Down, aes(label = delabel), color = "black", max.overlaps = 20) +
-  # geom_text_repel(data = df_top50_log2ratio_Up, aes(label = delabel), color = "black", max.overlaps = 20) +
-  # geom_text_repel(data = df_top50_FDR_Down, aes(label = delabel), color = "black", max.overlaps = 20) +
-  # geom_text_repel(data = df_top50_FDR_Up, aes(label = delabel), color = "black", max.overlaps = 20) +
   theme_par() +
   labs(title = "2B vs NT 3d-Dox",
        x = expression ("Log"[2] ~ "FC"),
@@ -85,37 +57,12 @@ processed_data_5day <- atac_data_5day %>%
 
 table(processed_data_5day$DiffExp)
 
-# Label genes based on differential expression
-processed_data_5day$delabel <- NA
-processed_data_5day$delabel[processed_data_5day$DiffExp != "Non-significant"] <- processed_data_5day$Gene[processed_data_5day$DiffExp != "Non-significant"]
-
-# Remove rows with NA in delabel
-labeled_data_5day <- processed_data_5day %>% na.omit(delabel)
-
-# Sort data by log2ratio for up and down-regulated genes
-df_sorted_log2ratio_Down_5day <- arrange(labeled_data_5day, log2ratio)
-df_sorted_log2ratio_Up_5day <- arrange(labeled_data_5day, desc(log2ratio))
-
-# Select top 50 down-regulated and up-regulated genes by log2ratio
-df_top50_log2ratio_Down_5day <- slice(df_sorted_log2ratio_Down_5day, 1:50)
-df_top50_log2ratio_Up_5day <- slice(df_sorted_log2ratio_Up_5day, 1:50)
-
-# Sort data by FDR for down-regulated genes
-df_sorted_FDR_Down_5day <- arrange(labeled_data_5day, FDR)
-
-# Select top 50 genes by FDR for down-regulated genes
-df_top50_FDR_Down_5day <- slice(df_sorted_FDR_Down_5day, 1:50)
-
 # Plot data with ggplot2 for 5-day data
 volcano_plot_5day <- ggplot(processed_data_5day, aes(log2ratio, -log10(FDR), color = DiffExp, label = delabel)) +
   geom_point() +
   scale_color_manual(values = c("blue", "red", "darkgrey"), name = NULL) +
   geom_vline(xintercept = c(-0.58, 0.58), col = "black", lwd = 0.5, lty = 2) +
   geom_hline(yintercept = -log10(0.05), col = "black", lwd = 0.5, lty = 2) +             
-  # Add gene labels (uncomment the following lines if needed)
-  # geom_text_repel(data = df_top50_log2ratio_Down_5day, aes(label = delabel), color = "black", max.overlaps = 20) +
-  # geom_text_repel(data = df_top50_log2ratio_Up_5day, aes(label = delabel), color = "black", max.overlaps = 20) +
-  # geom_text_repel(data = df_top50_FDR_Down_5day, aes(label = delabel), color = "black") +
   theme_par() +
   labs(title = "2B vs NT 5d-Dox",
        x = expression ("Log"[2] ~ "FC"),
